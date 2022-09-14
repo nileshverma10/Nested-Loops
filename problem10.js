9c5a1e83471d375b54c61b832d522d5e21a573bb
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2022-09-14 18:35:48 +0530

    Add files via upload

--- problem10.js
+++ problem10.js
@@ -1,4 +1,19 @@
-for(i=10; i>=1; i--){
-  console.log(i);
-}
-
+function primeLoopCheck(num){
+	//Enter Code Here
+    	for(i=2; i<=num; i++){
+	    for(j=2; j<=i; j++){
+	        if(i%2==0){
+	            continue;
+	        }
+    	        if(i%j==0){
+	            break;
+	        }
+	    }
+	
+	if(i==j){
+	    console.log(j);
+	}
+	}
+   
+ }
+ 
